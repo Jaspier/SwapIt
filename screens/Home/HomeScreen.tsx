1b29261c7e3e20dcb4c42312a333d74e7334ccd8
40253397 <adungo01@qub.ac.uk>
2022-11-03 01:54:37 +0000

    Getting react-native-deck-swiper to work for swipeable cards functionality

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -2,9 +2,45 @@ import { TouchableOpacity } from "react-native";
 import React from "react";
 import AuthenticationContext from "../../hooks/authentication/authenticationContext";
 import { SafeArea } from "../../components/utilities";
-import { HeaderContainer, AvatarIcon, Logo } from "./homeStyles";
+import {
+  HeaderContainer,
+  AvatarIcon,
+  Logo,
+  DeckSwiper,
+  SwiperContainer,
+  Card,
+  CardImage,
+} from "./homeStyles";
 import { Ionicons } from "@expo/vector-icons";
 import { colors } from "../../theme/colors";
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    displayName: "Allen Dungo",
+    itemName: "Variegated Monstera",
+    location: "Antrim",
+    photoURLs: [
+      "http://static1.squarespace.com/static/5a032c682278e74417b38113/t/5d3647e5339e9100019b9557/1563838454790/monvar.jpg?format=1500w",
+    ],
+  },
+  {
+    id: 2,
+    displayName: "Jasper Davidson",
+    itemName: "Golden Pothos",
+    location: "Belfast",
+    photoURLs: ["https://media.bunches.co.uk/products/ptempp-1.jpg"],
+  },
+  {
+    id: 3,
+    displayName: "Olivia Craig",
+    itemName: "Succulent",
+    location: "Ballymena",
+    photoURLs: [
+      "https://www.bhg.com/thmb/CV-IdGD-c0WFMRCsFgj-_tUnelo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/panda-plant-ee6cc069-72de16b817654cfe944b1f3055420f3e.jpg",
+    ],
+  },
+];
 
 const HomeScreen = ({ navigation }: any) => {
   const authContext = AuthenticationContext();
@@ -35,6 +71,19 @@ const HomeScreen = ({ navigation }: any) => {
         </TouchableOpacity>
       </HeaderContainer>
       {/* End of Header */}
+
+      {/* Start of Cards Section */}
+      <SwiperContainer>
+        <DeckSwiper
+          cards={DUMMY_DATA}
+          renderCard={(card: any) => (
+            <Card key={card.id}>
+              <CardImage source={{ uri: card.photoURLs[0] }} />
+            </Card>
+          )}
+        />
+      </SwiperContainer>
+      {/* End of Cards Section */}
     </SafeArea>
   );
 };
--- screens/Home/homeStyles.tsx
+++ screens/Home/homeStyles.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { colors } from "../../theme/colors";
 import { Avatar } from "react-native-paper";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import Swiper from "react-native-deck-swiper";
 
 export const HeaderContainer = styled(View)`
   flex-direction: row;
@@ -22,4 +23,28 @@ export const Logo = styled(Text)`
   font-size: 25px;
   font-weight: 500;
   color: ${colors.brand.primary};
+`;
+
+export const SwiperContainer = styled(View)`
+  flex: 1;
+  margin-top: -24px;
+`;
+
+export const DeckSwiper = styled(Swiper).attrs({
+  containerStyle: { backgroundColor: "transparent" },
+})``;
+
+export const Card = styled(View)`
+  position: relative;
+  background-color: white;
+  height: 75%;
+  border-radius: 12px;
+`;
+
+export const CardImage = styled(Image)`
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  border-radius: 12px;
 `;
