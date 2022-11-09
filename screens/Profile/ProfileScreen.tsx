e8b929aebfbceb59b24decbfb2590b6cfc2d263e
40253397 <adungo01@qub.ac.uk>
2022-11-09 15:11:13 +0000

    Getting selecting of images from device working

--- screens/Profile/ProfileScreen.tsx
+++ screens/Profile/ProfileScreen.tsx
@@ -1,18 +1,46 @@
-import { Text, Button } from "react-native";
-import React from "react";
+import { Text, Button, Image, FlatList } from "react-native";
+import React, { useState } from "react";
 import AuthenticationContext from "../../hooks/authentication/authenticationContext";
 import { SafeArea } from "../../components/utilities";
+import * as ImagePicker from "expo-image-picker";
 
 const ProfileScreen = () => {
+  const [images, setImages] = useState<ImagePicker.ImageInfo[] | null>(null);
   const authContext = AuthenticationContext();
   if (!authContext) {
     return null;
   }
   const { logout }: AuthContextInterface = authContext;
 
+  const pickImages = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      selectionLimit: 3,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImages(result.selected);
+    }
+  };
+
   return (
     <SafeArea>
       <Text>ProfileScreen</Text>
+      <Button title="Pick an image from camera roll" onPress={pickImages} />
+      <FlatList
+        horizontal={true}
+        data={images}
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item.uri }}
+            style={{ width: 200, height: 200 }}
+          />
+        )}
+      />
       <Button title="Logout" onPress={() => logout()} />
     </SafeArea>
   );
