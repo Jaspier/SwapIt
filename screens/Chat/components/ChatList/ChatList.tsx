ec856156bcd786bc055cf4bd02bc475f2656ddc8
40253397 <adungo01@qub.ac.uk>
2023-01-22 00:09:26 +0000

    Implemented feature to delete match on swipe left of chatList row component

--- screens/Chat/components/ChatList/ChatList.tsx
+++ screens/Chat/components/ChatList/ChatList.tsx
@@ -1,10 +1,19 @@
-import { FlatList } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../../../../firebase";
 import AuthenticationContext from "../../../../hooks/authentication/authenticationContext";
 import ChatRow from "../ChatRow/ChatRow";
 import { NoMatchesContainer, NoMatchesText } from "./ChatListStyles";
+import { Swipeable } from "react-native-gesture-handler";
+import generateId from "../../../../lib/generateId";
 
 const ChatList = () => {
   const authContext = AuthenticationContext();
@@ -13,6 +22,20 @@ const ChatList = () => {
     return null;
   }
   const { user }: AuthContextInterface = authContext;
+
+  const deleteMatch = async (usersMatched: string[]) => {
+    if (user) {
+      await deleteDoc(
+        doc(db, "users", usersMatched[0], "swipes", usersMatched[1])
+      );
+      await deleteDoc(
+        doc(db, "users", usersMatched[1], "swipes", usersMatched[0])
+      );
+      await deleteDoc(
+        doc(db, "matches", generateId(usersMatched[0], usersMatched[1]))
+      );
+    }
+  };
 
   useEffect(() => {
     let unsub;
@@ -34,12 +57,37 @@ const ChatList = () => {
     return unsub;
   }, [user]);
 
+  const RightActions = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: "red",
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
+          width: 85,
+          height: 85,
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold" }}>Delete</Text>
+      </View>
+    );
+  };
+
   return matches.length > 0 ? (
     <FlatList
       style={{ height: "100%" }}
       data={matches}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <ChatRow matchDetails={item} />}
+      renderItem={({ item }) => (
+        <Swipeable
+          renderRightActions={() => <RightActions />}
+          //@ts-ignore
+          onSwipeableRightOpen={() => deleteMatch(item.usersMatched)}
+        >
+          <ChatRow matchDetails={item} />
+        </Swipeable>
+      )}
     />
   ) : (
     <NoMatchesContainer>
