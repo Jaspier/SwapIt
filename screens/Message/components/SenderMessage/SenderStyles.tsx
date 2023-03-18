49d4a913607c341793b2d12a411ad7d6012b49d4
40253397 <adungo01@qub.ac.uk>
2023-03-18 19:35:46 +0000

    Updated colour palette to a more pastel tone

--- screens/Message/components/SenderMessage/SenderStyles.tsx
+++ screens/Message/components/SenderMessage/SenderStyles.tsx
@@ -3,7 +3,7 @@ import { colors } from "../../../../theme/colors";
 import { View, Text } from "react-native";
 
 export const SenderMessageBubble = styled(View)`
-  background-color: rgb(147, 51, 234);
+  background-color: ${colors.brand.secondary};
   border-radius: ${(props) => props.theme.sizes[0]};
   border-top-right-radius: 0px;
   padding: ${(props) => props.theme.space[3]} ${(props) => props.theme.space[5]};
